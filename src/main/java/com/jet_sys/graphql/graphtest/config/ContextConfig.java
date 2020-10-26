0cc5352c22e968a4530f03637491c7ae3701c237
Terry <ethutch@gmail.com>
2020-10-26 19:58:03 -0400

    Basic changes to get hibernate load errors fixed.

---
+++ src/main/java/com/jet_sys/graphql/graphtest/config/ContextConfig.java
@@ -0,0 +1,46 @@
+package com.jet_sys.graphql.graphtest.config;
+
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class ContextConfig implements WebMvcConfigurer {
+
+
+	// H2
+//	@Bean
+//	public ServletRegistrationBean h2servletRegistration() {
+//		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+//		registration.addUrlMappings("/console/*");
+//		return registration;
+//	}
+
+	// Setup the Internationalization
+	@Bean
+	public SessionLocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.US);
+		return slr;
+	}
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+
+}
